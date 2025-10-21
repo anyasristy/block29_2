11e411a820a8a9bcf05a2516cc798ce3b26b874b
anyasristy <148508255+anyasristy@users.noreply.github.com>
2025-10-21 12:27:58 -0400

    commit

--- src/api/activities.js
+++ src/api/activities.js
@@ -35,3 +35,16 @@ export async function createActivity(token, activity) {
     throw Error(result.message);
   }
 }
+export async function deleteActivity(token,id){
+  if(!token){
+    throw Error("Please sign in to delete activity.");
+  }
+  const response=await fetch(API+"/activities/"+id,{
+    method: "DELETE",
+    headers:{Authorization:"Bearer"+token},
+  });
+  if(!response.ok){
+    const result=await response.json();
+    throw Error(result.message);
+  }
+}
